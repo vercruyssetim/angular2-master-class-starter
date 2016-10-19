c46c662e1b535d4bce055c6e13a9a34a72c74154
vercruyssetim <tim.vercruysse@cegeka.be>
2016-10-19 13:24:15 +0200

    [Tim] - excercise 2

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CONTACT_DATA} from './data/contact-data';
+import {Contact} from "./models/contact";
 
 @Component({
   selector: 'trm-contacts-app',
-  template: '<trm-contacts-header></trm-contacts-header>',
+  templateUrl: 'contacts.component.html',
   styleUrls: ['contacts.component.css']
 })
 export class ContactsAppComponent {
+  contacts: Array<Contact> = CONTACT_DATA;
 }
